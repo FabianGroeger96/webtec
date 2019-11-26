d4cfaec9d48ea891f1ed4469aa82550461cfa0ae
FabianGroeger96 <fabian.groeger@bluewin.ch>
2019-11-26 11:08:52 +0100

    added cookie to website

--- js/main.js
+++ js/main.js
@@ -22,3 +22,42 @@ $(document).ready(function () {
         }
     });
 });
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function removeCookie(cname) {
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    $('#alert-cookie').hide();
+}
+
+function getCookie(cname) {
+    const name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let last_visit = getCookie("last_visit");
+    if (last_visit !== "") {
+        $("#span-last-visit").text(last_visit);
+        $('#alert-cookie').show();
+    } else {
+        const current_date = new Date().toDateString();
+        setCookie("last_visit", current_date, 365);
+        $('#alert-cookie').hide();
+    }
+}
